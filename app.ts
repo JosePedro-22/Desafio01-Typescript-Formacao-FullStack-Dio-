3d2abf54d78409dfe2911944b9435b23040bfbd3
José Pedro S. Mesquita <jp14274@gmail.com>
2023-02-27 17:48:14 -0300

    first commit

--- app.ts
+++ app.ts
@@ -1,9 +1,13 @@
-import { CompanyAccount } from './class/CompanyAccount'
-import { PeopleAccount } from './class/PeopleAccount'
+import { CompanyAccount } from "./class/CompanyAccount";
+import { PeopleAccount } from "./class/PeopleAccount";
 
-const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
-const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+const peopleAccount: PeopleAccount = new PeopleAccount(1, "José Pedro", 25);
+//console.log(peopleAccount);
+peopleAccount.deposit(2500);
+peopleAccount.withdraw(3000);
+const companyAccount: CompanyAccount = new CompanyAccount(
+  "DIO.FormacaoTypescript",
+  20
+);
+companyAccount.deposit(500);
+//console.log(companyAccount);
